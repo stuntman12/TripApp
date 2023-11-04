e70278350280497a92e93aaa955ebc7e558ac0c5
Daniil <ivanov_daniil_a@vk.com>
2023-11-04 15:57:41 +0300

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by Даниил on 04.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
